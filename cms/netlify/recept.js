b80e8b02e19672bb62955e64fc000706fa02c932
JackDownload <kismocsy@gmail.com>
2020-02-09 08:49:30 +0100

    Create recept.js

---
+++ cms/netlify/recept.js
@@ -0,0 +1,20 @@
+import { getMixins } from './mixins'
+
+class Recept {
+  constructor(axios) {
+    this.slug = 'post'
+    this.slugPlural = 'receptek'
+    this.pretty = 'Recept'
+    this.plural = 'Receptek'
+    this.axios = axios
+    this.editUrl = '/admin/#/collections/receptek/entries'
+  }
+  getEditUrl(slug) {
+    return `${this.editUrl}/${slug}`
+  }
+}
+
+// Mixins
+Object.assign(Recept.prototype, getMixins)
+
+export default Recept
