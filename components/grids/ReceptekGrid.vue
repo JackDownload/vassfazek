28de77fa2ec4dcd9cfe74113995804aab22c4904
JackDownload <kismocsy@gmail.com>
2020-02-09 09:00:17 +0100

    Create ReceptekGrid.vue

---
+++ components/grids/ReceptekGrid.vue
@@ -0,0 +1,37 @@
+<template>
+  <resource-grid
+    v-bind="$props"
+    :resource="$cms.recept"
+    :theme="$siteConfig.recepts.theme || $siteConfig.cards.theme || null"
+  >
+    <template v-slot:default="{ item }">
+      <recept-card
+        :title="item.title"
+        :link="`/${item.slug}`"
+        :image="item.featureImage"
+        :author="item.author"
+        :date="item.date"
+      />
+    </template>
+  </resource-grid>
+</template>
+
+<script>
+import ReceptCard from '~/components/cards/ReceptCard'
+export default {
+  name: 'ReceptekGrid',
+  components: { ReceptCard },
+  props: {
+    perRow: { type: Number, default: 3 },
+    number: { type: Number, default: 0 },
+    order: { type: String, default: 'DESC' },
+    category: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    exclude: { type: String, default: '' }
+  }
+}
+</script>
